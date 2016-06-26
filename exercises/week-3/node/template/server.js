82874e2eceb8a1abf1e56ef4bd77eda2d6249d43
Chris Tyler <jchristyler@gmail.com>
2016-06-26 16:57:49 -0500

    Homework problem 3

--- exercises/week-3/node/template/server.js
+++ exercises/week-3/node/template/server.js
@@ -1,4 +1,34 @@
-
-// build a server that can server files from the public directory
-
-var http = require('http');
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const path = require('path');
+server = http.createServer(requestCallback);
+server.listen('3000', '127.0.0.1');
+function requestCallback(req, res){
+	console.log(req.url);
+	var urlPath = url.parse(req.url).pathname;
+	fileNotFound = () => {
+		res.writeHead(404, {'Content-Type': 'text/plain'});
+		res.end("File not found.");
+	}
+	if(urlPath.includes("..")) {
+		res.writeHead(400, {'Content-Type': 'text/plain'});
+		res.end("Bad request.");
+	} else {
+		var filePath = path.join("public", url.parse(req.url).pathname);
+		fs.stat(filePath, (err, stat) => {
+			if(err) {fileNotFound();}
+			else{
+				if(stat.isDirectory()) filePath = path.join(filePath, "index.html");
+				fs.readFile(filePath, (err, data) => {
+					if(err){
+						fileNotFound();
+					} else {
+						res.writeHead(200);
+						res.end(data);
+					}
+				});
+			}
+		});
+	}
+}
